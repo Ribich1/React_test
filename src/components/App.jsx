c8473e38570acd22884246d8354ad574cfda8e51
Ribich1 <ribich@ukr.net>
2023-07-11 19:06:23 +0200

    colorpicker+export default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
+import ColorPicker from './Counter/ColorPicker';
 import Counter from './Counter/Counter';
+import Dropdown from './Counter/Dropdown/Dropdown';
+
+const colorPickerOptions=[
+{label:'red', color:'#F44336'},
+{label:'green', color:'#4caf50'},
+{label:'blue', color:'#2196f3'},
+{label:'grey', color:'#607d8b'},
+{label:'pink', color:'#e91e63'},
+{label:'indigo', color:'#3f51b5'},
+];
+
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        // display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // fontSize: 40,
-     
-
-        color: '#010101',
-
-      }}
-    >
+    <>
       <h1>Состояние компонента</h1>
-      <Counter   />
-    </div>
+      {/* <Dropdown /> */}
+      {/* <Counter   /> */}
+      <ColorPicker options={colorPickerOptions} defaultIdx={3}/>
+    </>
   );
 };
